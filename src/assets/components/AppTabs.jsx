17d3f095d1d6c89566ef1d03d9f33be191794a09
pietrobertino <pietrobertino20@gmail.com>
2026-03-11 16:07:56 +0100

    fix repetions and add comments

--- src/assets/components/AppTabs.jsx
+++ src/assets/components/AppTabs.jsx
@@ -3,38 +3,42 @@ import MainCard from "./MainCard"
 
 export default function AppTabs({ languages }) {
 
-    const [show, setShow] = useState(0); // ho dato come valore iniziale l'id di html (primo linguaggio)
+    const [selected, setSelected] = useState(0);
 
     //dichiarando le seguenti variabili registro l'oggetto dell'array corrispondente all'id e accedo al suo valore associato alla chiave description e alla chaive title
-
-
-
-    let cardToShow = languages.find(language => { return language.id === show });
+    let cardToShow = languages.find(language => { return language.id === selected });
     let cardTitle = "";
     let cardDescription = "";
 
-    if (show != 0) {
+    //le variabili assumono un valore solo quando un linguaggio viene selezionato
+    if (selected != 0) {
         cardTitle = cardToShow.title;
         cardDescription = cardToShow.description;
     }
-
 
     return (
         <div className="container px-5">
             {/* Creo la lista di bottoni */}
 
             {languages.map(language => (
-                <button key={language.id} className=" btn btn-primary me-3 mb-3" onClick={() => setShow(language.id)}>{language.title}</button>
+                <button
+                    key={language.id}
+                    className={(language.id == selected) ? "btn btn-warning me-3 mb-3" : "btn btn-primary me-3 mb-3"} // in questo modo le classi del bottone dipendono se il bottone di questa iterazione è o non è quello selezionato
+                    onClick={() => setSelected(language.id)}> {/*al click di questo bottone vengono aggiornato il valore di selcted*/}
+                    {language.title}
+                </button>
             ))}
-            {/* in questo modo il valore di show è 1 all'inizio e diventa un altro corrispondente all'id di un linguaggio quando il relativo pulsante viene premuto */}
+            {/* in questo modo il valore di selected è 0 all'inizio e diventa un altro corrispondente all'id di un linguaggio quando il relativo pulsante viene premuto */}
 
             {/* Inserirsco all'interno della card la descrizione del linguaggio */}
             <div className="card p-3">
                 <div className=" card-title fs-3 fw-bolder">
-                    {(show != 0) ? cardTitle : "Nessun linguaggio selezionato"}
+                    {(selected != 0) ? cardTitle : "Nessun linguaggio selezionato"}
+                    {/* se selected è diverso da zero (un linguaggio è selezionato) mostra il titolo, altrimenti il messaggio di default */}
                 </div>
                 <div className=" card-text">
-                    {(show != 0) && cardDescription}
+                    {(selected != 0) && cardDescription}
+                    {/* se un linguaggio è selezionato mostrane la descrizione */}
                 </div>
             </div>
 
